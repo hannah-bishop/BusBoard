719d8857a51607c1158c36fe9950726e541f7e5a
hbishop1996@gmail.com <Hannah.Bishop@softwire.com>
2022-04-28 16:03:05 +0100

    a few minor changes

--- busboard.mjs
+++ busboard.mjs
@@ -10,16 +10,18 @@ const postcodeLat = postcodeInfo2.result.latitude;
 const nearestStop = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${postcodeLat}&lon=${postcodeLong}&stopTypes=NaptanPublicBusCoachTram`)
 const nearestStop2 = await nearestStop.json();
 const nearestStopsArray = nearestStop2.stopPoints;
-let nearestStopsArray2 = []
+let nearestStopsArray2 = [];
 
 nearestStopsArray.forEach(stop => nearestStopsArray2.push([stop.naptanId,stop.distance,stop.commonName,stop.indicator]));
 //console.log(nearestStopsArray2);
 console.log(`The two nearest stations are ${nearestStopsArray2[0][2]} (${nearestStopsArray2[0][3]}) and ${nearestStopsArray2[1][2]} (${nearestStopsArray2[1][3]})`)
 
 for (let i=0; i<2; i++){
+    console.log(`${nearestStopsArray2[i][2]} (${nearestStopsArray2[i][3]})`)
     const response = await fetch(`https://api.tfl.gov.uk/StopPoint/${nearestStopsArray2[i][0]}/Arrivals`);
     const arrivals = await response.json();
-    arrivals.sort((a,b) => a.timeToStation - b.timeToStation)
+    arrivals.sort((a,b) => a.timeToStation - b.timeToStation);
+    arrivals.slice(0,5);
     arrivals.forEach(arrival => console.log(`Bus to ${arrival.destinationName} is in ${arrival.timeToStation} seconds`));
 }
 
